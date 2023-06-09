1d2c398b3c8248f43490b92ebf935ca9e6c70fb6
Jaime L <jaimeee.loca@gmail.com>
2023-06-09 09:34:29 -0600

    initial api

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+// Express app
+const app = express();
+
+
+// ------------------------ Middleware ------------------------
+app.use(express.json()); // Para poder mandar datos al servidor en formato JSON
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// Para que cualquier dominio pueda acceder a la api
+app.use(cors({
+    origin: "*",
+}));
+
+
+// ------------------------ Routes ------------------------
+app.use('/api', routes);
+
+
+let port = 3006;
+
+// Listen for requests
+app.listen(port, () => {
+    console.log('Listening on port', port, '...');
+});
+
+//exports.api = functions.https.onRequest(app);
---
+++ routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// ----------------- Controllers -----------------
+//const {test} = require('./controllers/testController');
+
+router = express.Router();
+
+// ----------------- Rutas -----------------
+router.post('/', (req, res) => {
+    res.send("Hello World");
+});
+
+module.exports = router;
