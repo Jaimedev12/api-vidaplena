ba1e0d928bf4e41a9c4ac49eb4aec8f9447c3869
Jaime L <jaimeee.loca@gmail.com>
2023-06-09 11:32:36 -0600

    Controller terminado

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pamController = require('./controllers/pamController');
+const roleController = require('./controllers/roleController');
 
 // ----------------- Controllers -----------------
 //const {test} = require('./controllers/testController');
@@ -11,5 +12,13 @@ router.get('/', (req, res) => {
     res.send("Hello World!");
 });
 router.get('/pams', pamController.getPams);
+
+
+router.get('/get-roles', roleController.getRoles);
+router.get('/get-role/:id', roleController.getRoleById);
+router.post('/add-role', roleController.addRole);
+router.put('/edit-role/:id', roleController.editRoleById);
+router.delete('/delete-role/:id', roleController.deleteRoleById);
+
 
 module.exports = router;
