b685db7fdf46b48f49db4fd013ebd8658ed4adb9
FernandoBurgos <A01236284@tec.mx>
2023-06-12 15:20:26 -0600

    punto y comas faltantes

--- controllers/pamGroupController.js
+++ controllers/pamGroupController.js
@@ -4,7 +4,7 @@ class MainController {
 
     async getPamGroups(req, res) {
         console.log("Get Pam Groups");
-        var sql = `call sp_get_pam_groups()`;
+        var sql = `call sp_get_pam_groups();`;
         mysql.query(sql, (error, data, fields) => {
             if (error) {
                 res.status(500);
@@ -22,7 +22,7 @@ class MainController {
         console.log("Get Pam Group By Id");
         if (req.params.id != null) {
             let pam_group_id = req.params.id;
-            var sql = `call sp_get_pam_group_by_id(${pam_group_id})`;
+            var sql = `call sp_get_pam_group_by_id(${pam_group_id});`;
             mysql.query(sql, (error, data, fields) => {
                 if (error) {
                     res.status(500);
@@ -49,7 +49,7 @@ class MainController {
         let group_name = req.body.group_name;
         let group_parent_id = req.body.group_parent_id;
 
-        var sql = `call sp_add_pam_group(${group_type_id}, '${group_name}', ${group_parent_id})`;
+        var sql = `call sp_add_pam_group(${group_type_id}, '${group_name}', ${group_parent_id});`;
         mysql.query(sql, (error, data, fields) => {
             if (error) {
                 res.status(500);
@@ -70,16 +70,16 @@ class MainController {
         console.log("Edit Pam Group");
         console.log(req.body);
         if (
-            req.params.pam_group_id != null &&
+            req.params.id != null &&
             req.body.group_type_id != null &&
             req.body.group_name != null &&
             req.body.group_parent_id != null
             ){
-        let pam_group_id = req.params.pam_group_id;
+        let pam_group_id = req.params.id;
         let group_type_id= req.body.group_type_id;
         let group_name = req.body.group_name;
         let group_parent_id = req.body.group_parent_id;
-        var sql = `call sp_edit_pam_group_by_id(${pam_group_id}, ${group_type_id}, '${group_name}', ${group_parent_id})`;
+        var sql = `call sp_edit_pam_group_by_id(${pam_group_id}, ${group_type_id}, '${group_name}', ${group_parent_id});`;
         mysql.query(sql, (error, data, fields) => {
             if (error) {
                 res.status(500);
@@ -101,7 +101,7 @@ class MainController {
         console.log("Delete Pam Group");
         if (req.params.id != null) {
             let pam_group_id = req.params.id;
-            var sql = `call sp_delete_pam_group_by_id(${pam_group_id})`;
+            var sql = `call sp_delete_pam_group_by_id(${pam_group_id});`;
             mysql.query(sql, (error, data, fields) => {
                 if (error) {
                     res.status(500);
